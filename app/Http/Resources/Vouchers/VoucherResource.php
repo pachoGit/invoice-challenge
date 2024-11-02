67e7298d0e59ae84338e143e91f0d4634c270867
pachoGit <carlos.hcpv@gmail.com>
2024-11-02 14:00:29 -0500

    (Dev): Agregar filtros para el listado de vouchers

--- app/Http/Resources/Vouchers/VoucherResource.php
+++ app/Http/Resources/Vouchers/VoucherResource.php
@@ -26,6 +26,8 @@ class VoucherResource extends JsonResource
             'receiver_document_type' => $this->resource->receiver_document_type,
             'receiver_document_number' => $this->resource->receiver_document_number,
             'total_amount' => $this->resource->total_amount,
+            'serie' => $this->resource->serie,
+            'correlative' => $this->resource->correlative,
             'user' => $this->whenLoaded(
                 'user',
                 fn () => UserResource::make($this->resource->user),
